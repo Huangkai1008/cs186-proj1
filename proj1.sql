5fbab4b3b5195b3dee3ed79118a86c017e7a7f30
huangkai <h1770360848@outlook.com>
2024-06-13 21:57:07 +0800

    feat: task 1

--- proj1.sql
+++ proj1.sql
@@ -19,94 +19,107 @@ DROP VIEW IF EXISTS q4v;
 -- Question 0
 CREATE VIEW q0(era)
 AS
-  SELECT 1 -- replace this line
+SELECT MAX(era)
+FROM pitching
 ;
 
 -- Question 1i
 CREATE VIEW q1i(namefirst, namelast, birthyear)
 AS
-  SELECT 1, 1, 1 -- replace this line
+SELECT nameFirst, nameLast, birthYear
+FROM people
+WHERE weight > 300;
 ;
 
 -- Question 1ii
 CREATE VIEW q1ii(namefirst, namelast, birthyear)
 AS
-  SELECT 1, 1, 1 -- replace this line
+SELECT nameFirst, nameLast, birthYear
+FROM people
+WHERE nameFirst LIKE '% %'
+ORDER BY nameLast, nameFirst
 ;
 
 -- Question 1iii
 CREATE VIEW q1iii(birthyear, avgheight, count)
 AS
-  SELECT 1, 1, 1 -- replace this line
+SELECT birthYear, AVG(height), COUNT(*)
+FROM people
+GROUP BY birthYear
+ORDER BY birthYear
 ;
 
 -- Question 1iv
 CREATE VIEW q1iv(birthyear, avgheight, count)
 AS
-  SELECT 1, 1, 1 -- replace this line
+SELECT birthYear, AVG(height), COUNT(*)
+FROM people
+GROUP BY birthYear
+HAVING AVG(height) > 70
+ORDER BY birthYear
 ;
 
 -- Question 2i
 CREATE VIEW q2i(namefirst, namelast, playerid, yearid)
 AS
-  SELECT 1, 1, 1, 1 -- replace this line
+SELECT 1, 1, 1, 1 -- replace this line
 ;
 
 -- Question 2ii
 CREATE VIEW q2ii(namefirst, namelast, playerid, schoolid, yearid)
 AS
-  SELECT 1, 1, 1, 1, 1 -- replace this line
+SELECT 1, 1, 1, 1, 1 -- replace this line
 ;
 
 -- Question 2iii
 CREATE VIEW q2iii(playerid, namefirst, namelast, schoolid)
 AS
-  SELECT 1, 1, 1, 1 -- replace this line
+SELECT 1, 1, 1, 1 -- replace this line
 ;
 
 -- Question 3i
 CREATE VIEW q3i(playerid, namefirst, namelast, yearid, slg)
 AS
-  SELECT 1, 1, 1, 1, 1 -- replace this line
+SELECT 1, 1, 1, 1, 1 -- replace this line
 ;
 
 -- Question 3ii
 CREATE VIEW q3ii(playerid, namefirst, namelast, lslg)
 AS
-  SELECT 1, 1, 1, 1 -- replace this line
+SELECT 1, 1, 1, 1 -- replace this line
 ;
 
 -- Question 3iii
 CREATE VIEW q3iii(namefirst, namelast, lslg)
 AS
-  SELECT 1, 1, 1 -- replace this line
+SELECT 1, 1, 1 -- replace this line
 ;
 
 -- Question 4i
 CREATE VIEW q4i(yearid, min, max, avg)
 AS
-  SELECT 1, 1, 1, 1 -- replace this line
+SELECT 1, 1, 1, 1 -- replace this line
 ;
 
 -- Question 4ii
 CREATE VIEW q4ii(binid, low, high, count)
 AS
-  SELECT 1, 1, 1, 1 -- replace this line
+SELECT 1, 1, 1, 1 -- replace this line
 ;
 
 -- Question 4iii
 CREATE VIEW q4iii(yearid, mindiff, maxdiff, avgdiff)
 AS
-  SELECT 1, 1, 1, 1 -- replace this line
+SELECT 1, 1, 1, 1 -- replace this line
 ;
 
 -- Question 4iv
 CREATE VIEW q4iv(playerid, namefirst, namelast, salary, yearid)
 AS
-  SELECT 1, 1, 1, 1, 1 -- replace this line
+SELECT 1, 1, 1, 1, 1 -- replace this line
 ;
 -- Question 4v
 CREATE VIEW q4v(team, diffAvg) AS
-  SELECT 1, 1 -- replace this line
+SELECT 1, 1 -- replace this line
 ;
 
